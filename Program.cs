c3f414e2e5eb151a739a07e055e1b948dabd19ed
Sergey Dolin <sergey.dolin@gmail.com>
2022-04-04 11:00:27 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace dotnet_locales
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    CultureInfo[] cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            foreach (CultureInfo culture in cultures)
+            {
+               Console.WriteLine(culture.Name);
+            }
+        }
+    }
+}
